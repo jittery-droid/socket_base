0ac373276982ade4fc1a1f90f7de6c8028b2611d
jittery-droid <jittery.dev@gmail.com>
2021-01-16 00:40:22 -0600

    code cleanup

--- middleware/require_user.go
+++ middleware/require_user.go
@@ -58,16 +58,6 @@ func (mw *User) Apply(next http.Handler) http.HandlerFunc {
 
 func (mw *User) ApplyFn(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-		if r.Method == "OPTIONS" {
-			next(w, r)
-			return
-		}
 
 		path := r.URL.Path
 
